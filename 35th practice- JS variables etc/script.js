1992e3b75d511b27a4ef1607eca1c7dc8b348c62
G.H.O.S.T <kaushikofficial.agt@gmail.com>
2024-02-06 21:48:10 +0530

    JS variables, data types

---
+++ 35th practice- JS variables etc/script.js
@@ -0,0 +1,39 @@
+console.log("i an learning JS")
+
+var a=5
+var b=6
+var c="kaushik"
+
+{
+    let a=69
+    a+=1
+    console.log(a)
+}
+
+console.log(a)
+console.log(typeof a,typeof b, typeof c)
+
+
+let a1=null
+let a2=56
+let a3=7.9
+const a4=true
+let a5=undefined
+let a6="kaushik"
+
+console.log(typeof a1, typeof a2, typeof a3, typeof a4, typeof a5, typeof a6)
+
+let o={
+    name:"ghost",
+    "job role":"developer",
+    "obidient":false
+}
+
+console.log(o)
+console.log(typeof o)
+o.exp="0 years"
+console.log(o)
+o.num=23
+console.log(o)
+o.num+=30
+console.log(o)
