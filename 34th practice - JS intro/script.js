484267e4249fda96606b0a0716a431b18b396524
G.H.O.S.T <kaushikofficial.agt@gmail.com>
2024-02-06 19:50:43 +0530

    JS intro

---
+++ 34th practice - JS intro/script.js
@@ -0,0 +1,22 @@
+alert("Hello!!")
+
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is also also running")
+
+var a=prompt("koi number dalo?")
+
+
+var b=confirm("do u want to die?")
+
+if(b){
+    console.log("you are dead")
+}
+else{
+    console.log("you are alive")
+}
+
+console.log("apka number hai " + a)
+
+document.title="Ghost's Website"
+document.body.style.backgroundColor="red"
